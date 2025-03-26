a1ce93b6d2805dcedd60fab346f8d29805b19604
D4rthDistortion <stephenwhitley07@gmail.com>
2025-03-26 18:03:02 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
+/*
+example of running a function for "robot to get milk from store" and rounding the number of bottle it can get with 5 dollars
 
+function getMilk(money) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var numberOfBottles = Math.floor(money / 1.5);
+  console.log("buy " + numberOfBottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+
+getMilk(5);
+
+*/
